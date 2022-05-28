c89f3232a9e327c39dd8ac94a84592962aa2e239
leopempem <leopemribeiro@gmail.com>
2022-05-28 18:44:09 -0300

    container 2

---
+++ index.js
@@ -0,0 +1,13 @@
+// JAVASCRIPT
+$(document).ready(function() {
+	$('#subir').click(function(){
+		$('html, body').animate({scrollTop:0}, 'slow');
+		return false;
+	});
+});
+
+
+/* var btn = document.querySelector("#back-to-top");
+btn.addEventListener("click", function() {
+    window.scrollTo(0, 0);
+}); */
