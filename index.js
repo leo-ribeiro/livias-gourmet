a8d742d801ea98a0e83f8f21ce24f5e0450f0f81
leopempem <leopemribeiro@gmail.com>
2022-06-04 18:48:28 -0300

    Botão voltar ao topo

---
+++ index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $(".back-to-top").css("display", "none");
+  $("body").scroll(function() {
+      if($(this).scrollTop() == 0){
+        $(".back-to-top").css("display", "none");
+      } else {
+        $(".back-to-top").css("display", "block");
+      }
+    });
+  $(".back-to-top").click(function() {
+      $("html, body").animate({scrollTop: 0}, 800);
+     });
+  });
